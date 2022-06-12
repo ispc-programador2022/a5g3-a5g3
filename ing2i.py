8f6c6d9254747b5120a576d0155c4db7f1bac653
tomasescobar24 <tomasecobar24@gmail.com>
2022-06-12 21:18:54 +0000

    Subiendo función de numeros enteros

---
+++ ing2i.py
@@ -0,0 +1,7 @@
+#Funcion que permite el ingreso de dos numeros enteros
+
+def ing2i():
+    a = int(input("Ingrese el primer valor entero: "))
+    b = int(input("Ingrese el segundo valor entero: "))
+    print("los numeros enteros ingresados son: ",a, " y ",b)
+ing2i()
