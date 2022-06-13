0878930febd52717542aa3ebdd8312714d2e78f8
IAferaboli <iagferaboli@gmail.com>
2022-06-13 15:41:29 +0000

    first commit

---
+++ funciones/ing2s.py
@@ -0,0 +1,8 @@
+#Función que permita el ingreso de 2 valores string.
+
+def ing2s():
+  a= input("Ingresar primer palabra: ")
+  b= input("Ingresar segunda palabra: ")
+  print("Las palabras ingresadas son: ",a,b)
+  
+ing2s()
