1cb81fa34058b046e5f1d3536a708a97593d1650
IAferaboli <iagferaboli@gmail.com>
2022-06-15 10:44:21 +0000

    first commit

--- funciones/ing2s.py
+++ funciones/ing2s.py
@@ -3,6 +3,4 @@
 def ing2s():
   a= input("Ingresar primer palabra: ")
   b= input("Ingresar segunda palabra: ")
-  print("Las palabras ingresadas son: ",a,b)
   
-ing2s()
--- funciones/potencia.py
+++ funciones/potencia.py
@@ -1,6 +1,4 @@
 #Función que retorna la potencia del primero elevado al segundo parámetro.
 
-def potencia():
-  base= int(input("Ingrese la base: "))
-  exponente= int(input("Ingrese el exponente: "))
-  return base**exponente
+def potencia(a, b):
+  return a**b
