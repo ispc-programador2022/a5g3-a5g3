a72afa9fc3760a081218d448a262fc7ef08219c3
Bruno Fioranelli <fioranellibruno@gmail.com>
2022-06-17 21:53:42 -0300

    segundo commit

--- funciones/genrnd.py
+++ funciones/genrnd.py
@@ -1,6 +1,15 @@
-"""
-12- función genrnd que retorna una lista con 50 números aleatorios.
-"""
+import random
 
 def genrnd():
-    print("hola")
+    """retorna una lista con 50 números aleatorios"""
+    
+    cant_numeros = 50 # cantidad de números a generar
+    desde = 1
+    hasta = 1000
+
+    lista = []
+    for i in range(cant_numeros):
+        num_aleatorio = random.randint(desde, hasta)
+        lista.append(num_aleatorio)
+    
+    return lista
