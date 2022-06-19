1ffb9680dc4718fb2770bbebce042ed3b3af5b90
ferrsil <105982332+ferrsil@users.noreply.github.com>
2022-06-19 18:19:55 -0300

    Add files via upload

---
+++ funciones/super_rango.py
@@ -0,0 +1,16 @@
+from super_genrnd import
+
+def rango():
+    valor_max = None
+    valor_min = None
+    for num in lista_rnd:
+        if (valor_max is None or num > valor_max):
+            valor_max = num
+    
+        if (valor_min is None or num < valor_min):
+            valor_min = num
+
+    rango_Mm = valor_max - valor_min
+    print('El rango de la lista es: ',rango_Mm)
+    return
+    
