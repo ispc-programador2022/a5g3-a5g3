64e6b4d76b927984f1dc9c3d4131ed30e889bdf9
silelizondo <105612205+silelizondo@users.noreply.github.com>
2022-06-20 15:54:06 -0300

    Add files via upload

---
+++ funciones/7_Radicacion.py
@@ -0,0 +1,6 @@
+#Radicacion dos valores 
+print('La radicación consta de dos parámetros, un radicando y un índice')
+a = float(input("Inserte el radicando: "))
+b = int(input("Inserte el índice: "))
+c = a**(1/b)
+print("La raíz", b, "de", a, "es", c)
